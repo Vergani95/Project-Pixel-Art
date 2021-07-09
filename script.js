c0e9d84c52a07e13a1211f23e070275eeebd1e07
Vini_Vergani <vinivergani95@gmail.com>
2021-07-09 14:09:33 -0300

    continua as alterações do item 2

---
+++ script.js
@@ -0,0 +1,12 @@
+const pallet = {
+  colors = document.getElementsByClassName('color'),
+  cor0 = document.querySelector('#color0'),
+  cor1 = document.querySelector('#color1'),
+  cor2 = document.querySelector('#color2'),
+  cor3 = document.querySelector('#color3'),
+
+}
+
+
+
+
