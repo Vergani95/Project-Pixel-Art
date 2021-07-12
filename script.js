b24e267f7c388458bb976e2cd012ac5c1111785d
Vini_Vergani <vinivergani95@gmail.com>
2021-07-12 18:00:18 -0300

    adicona o requisito 9'

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ const table = document.querySelector("#pixel-board"); // Para o desenvolvimento 
   }
 }
 
+//Seleciona cor
 for (let indexF = 0; indexF < colors.length; indexF += 1) {
   colors[indexF].addEventListener('click', () => {
     for (let indexI = 0; indexI < colors.length; indexI += 1) {
@@ -31,9 +32,21 @@ for (let indexF = 0; indexF < colors.length; indexF += 1) {
   });
 }
 
+//Muda cor
 const square = document.getElementsByClassName("pixel");
 for (let indexP = 0; indexP < square.length; indexP += 1) {
   square[indexP].addEventListener('click', () => {
     square[indexP].style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
   });
 }
+
+//Botão Clear
+function whipeColors() {
+  const boardPixels = Array.from(document.querySelectorAll('.pixel'));
+  for (let indexW = 0; indexW < boardPixels.length; indexW += 1) {
+    boardPixels[indexW].style.backgroundColor = 'white';
+  }
+}
+
+const clearButton = document.querySelector('#clear-board');
+clearButton.addEventListener('click', whipeColors);
