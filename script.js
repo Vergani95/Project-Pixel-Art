0f7e6c9eec52c65072cd54539906265d66037ec6
Vini_Vergani <vinivergani95@gmail.com>
2021-07-12 18:05:13 -0300

    Altera lint do js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const colors = document.getElementsByClassName('color');
 
 const cor0 = document.querySelector('#color0');
@@ -10,19 +9,18 @@ cor2.style.backgroundColor = 'blueviolet';
 const cor3 = document.querySelector('#color3');
 cor3.style.backgroundColor = 'red';
 
-
 const table = document.querySelector("#pixel-board"); // Para o desenvolvimento deste codigo foi utilizado a estrutura/logica basica do exemplo visto na aula 5.4 onde se criava uma piramede de forma dinâmica
-  for (let indexL = 0; indexL < 5; indexL += 1) { //para alterar o tamanho da tabela mudar a variavel
-    const line = document.createElement('tr');
-    table.appendChild(line);
+for (let indexL = 0; indexL < 5; indexL += 1) { //para alterar o tamanho da tabela mudar a variavel
+  const line = document.createElement('tr');
+  table.appendChild(line);
   for (let indexC = 0; indexC < 5; indexC += 1) {
-    const columm = document.createElement ('td')
+    const columm = document.createElement('td');
     columm.className = "pixel";
     line.appendChild(columm);
   }
 }
 
-//Seleciona cor
+// Seleciona cor
 for (let indexF = 0; indexF < colors.length; indexF += 1) {
   colors[indexF].addEventListener('click', () => {
     for (let indexI = 0; indexI < colors.length; indexI += 1) {
@@ -32,7 +30,7 @@ for (let indexF = 0; indexF < colors.length; indexF += 1) {
   });
 }
 
-//Muda cor
+// Muda cor
 const square = document.getElementsByClassName("pixel");
 for (let indexP = 0; indexP < square.length; indexP += 1) {
   square[indexP].addEventListener('click', () => {
@@ -40,11 +38,11 @@ for (let indexP = 0; indexP < square.length; indexP += 1) {
   });
 }
 
-//Botão Clear
+// Botão Clear
 function whipeColors() {
   const boardPixels = Array.from(document.querySelectorAll('.pixel'));
   for (let indexW = 0; indexW < boardPixels.length; indexW += 1) {
-    boardPixels[indexW].style.backgroundColor = 'white';
+    boardPixels[indexW].style.backgroundColor = 'rgb(255, 255, 255)';
   }
 }
 
